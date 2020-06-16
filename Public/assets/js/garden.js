d6cdcfdcc6c8ac6962bef640f45aed8e04f0f85e
Tingting Chen <tingtingmacpro@Tingtings-MacBook-Pro.local>
2020-06-15 23:41:55 -0700

    draggable plants

--- Public/assets/js/garden.js
+++ Public/assets/js/garden.js
@@ -6,7 +6,7 @@ const garden = [
         daysToMature: 50,
         maturity:70,
         img: "assets/TT Images/parsley.png",
-        positionTop: -70,
+        positionTop: 0,
         positionLeft: 100,
     },
     {
@@ -15,7 +15,7 @@ const garden = [
         dayPlanted: 1589932800,
         maturity:50,
         img: "assets/TT Images/basil.png",
-        positionTop: -70,
+        positionTop: 0,
         positionLeft: 200,
     },
     {
@@ -24,7 +24,7 @@ const garden = [
         dayPlanted: 1589932800,
         maturity:180,
         img: "assets/TT Images/rosemary.png",
-        positionTop: -70,
+        positionTop: 0,
         positionLeft: 300,
     },
     {
@@ -33,7 +33,7 @@ const garden = [
         dayPlanted: 1589932800,
         maturity:21,
         img: "assets/TT Images/cilantro.png",
-        positionTop: -70,
+        positionTop: 0,
         positionLeft: 400,
     },
     {
@@ -42,27 +42,27 @@ const garden = [
         dayPlanted: 1589932800,
         maturity:60,
         img: "assets/TT Images/pepper.png",
-        positionTop: -70,
+        positionTop: 0,
         positionLeft: 500,
     }
 ]
 
-const grids = {
-    1:{
+// const grids = {
+//     1:{
 
-    },
-    2:null,
+//     },
+//     2:null,
     
-}
+// }
 
 const date = new Date();
 console.log(date.valueOf());
 
 for (i=0; i<garden.length; i++) {
     let daysToMature = Math.min(~~((date.valueOf()/1000 - garden[i].dayPlanted)/86400), garden[i].maturity);
-    let height = daysToMature/garden[i].maturity * 200;
+    let height = Math.max(daysToMature/garden[i].maturity * 200, 40);
     $("#maturity-div").append(`<p>${garden[i].name}: ${daysToMature}/${garden[i].maturity}<p>`);
-    $("#gardendiv").append(`<img class="plants" src="${garden[i].img}" style="position:fixed; top:${garden[i].positionTop}px; left:${garden[i].positionLeft}px; height:${height}px">`);
+    $("#gardendiv").append(`<img id="plant${i}" draggable="true" ondragstart="drag_start(event)" class="plants" src="${garden[i].img}" style="position:fixed; top:${garden[i].positionTop}px; left:${garden[i].positionLeft}px; height:${height}px">`);
 }
 
 
@@ -74,6 +74,21 @@ $(document).ready(function() {
     }); 
 
 
+    function allowDrop(ev) {
+        ev.preventDefault();
+      }
+      
+      function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+      }
+      
+      function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+      }
+      
+
     // to close virtual garden
     $(document).click(function(event){
       $("#whiteout").attr("style", "display: none");
@@ -84,3 +99,5 @@ $(document).ready(function() {
     });
 
 })
+
+
